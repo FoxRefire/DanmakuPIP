eee674dcf6e7ab86aa96b73935ba60d59fcac7dc
FoxRefire <155989196+FoxRefire@users.noreply.github.com>
2025-08-14 17:45:32 +0900

    Initial beta release

---
+++ README.md
@@ -0,0 +1,16 @@
+<p align="center">
+  <img width="180" src="./icon.png">
+  <h1 align="center">DanmakuPIP</h1>
+  <div align="center">Show Danmaku comments in Picture-in-Picrure widow</div>
+</p>
+
+> [!WARNING]
+> **This extension is experimental. It contains some bugs and currently only supports Niconico Douga.**
+> **This will be fixed in the future.**
+
+## Supported websites
+```
+www.nicovideo.jp
+```
+## Screenshot
+<img width="1000" alt="image" src="https://github.com/user-attachments/assets/a2663526-52a4-4540-bc80-d48ef384a9be" />
---
+++ nicovideo.user.js
@@ -0,0 +1,95 @@
+// ==UserScript==
+// @name        DanmakuPIP Niconico
+// @namespace   Violentmonkey Scripts
+// @match       https://www.nicovideo.jp/watch/*
+// @grant       none
+// @version     1.0
+// @author      FoxRefire
+// @description Show Danmaku comments in Picture-in-Picrure widow
+// ==/UserScript==
+
+async function waitTargetElements() {
+    return new Promise(resolve => {
+        let interval = setInterval(_ => {
+            let videoElm = document.querySelector("div[data-name=content] video")
+            let canvasElm = document.querySelector("div[data-name=comment] canvas")
+            if(canvasElm && videoElm.videoWidth) {
+                resolve([videoElm, canvasElm])
+                clearInterval(interval)
+            }
+        }, 100)
+    })
+}
+
+async function main() {
+    let originalElems = await waitTargetElements()
+    let newSource = createSource(originalElems)
+    console.log(newSource)
+
+    let newVideo = createVideo(originalElems[0], newSource)
+    console.log(newVideo)
+    originalElems[0].after(newVideo)
+    originalElems.map(e => e.style.display = "none")
+
+}
+
+function createSource(originalElems) {
+    let newCanvas = document.createElement("canvas")
+    let ctx = newCanvas.getContext("2d")
+
+    newCanvas.width = originalElems[0].videoWidth
+    newCanvas.height = originalElems[0].videoHeight
+    draw(newCanvas, ctx, originalElems)
+    let videoStream = newCanvas.captureStream(30)
+    let audioStream = getAudioStream(originalElems[0])
+    let mixedStream = new MediaStream([
+        ...videoStream.getVideoTracks(),
+                                      ...audioStream.getAudioTracks()
+    ])
+    let mediaSource = new MediaSource()
+    mediaSource.addEventListener('sourceopen', () => {
+        let sourceBuffer = mediaSource.addSourceBuffer('video/webm')
+        mediaSource.duration = originalElems[0].duration
+
+        let recorder = new MediaRecorder(mixedStream, { mimeType: 'video/webm' })
+        recorder.ondataavailable = e => {
+            if (e && e.data && e.data.size > 0) {
+                e.data.arrayBuffer().then(buf => sourceBuffer.appendBuffer(buf))
+            }
+        }
+        recorder.start(500)
+    })
+
+    return mediaSource
+}
+
+function createVideo(originalVideo, mediaSource) {
+    let newVideo = originalVideo.cloneNode()
+    newVideo.src = URL.createObjectURL(mediaSource)
+    newVideo.onplay = async () => await originalVideo.play()
+    newVideo.onpause = () => originalVideo.pause()
+    originalVideo.onplay = () => newVideo.play()
+    originalVideo.onpause = () => newVideo.pause()
+    originalVideo.classList.add("librezamFlag")
+    return newVideo
+}
+
+function draw(canvas, ctx, originalElems) {
+    try {
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+        ctx.drawImage(originalElems[0], 0, 0, canvas.width, canvas.height)
+        ctx.drawImage(originalElems[1], 0, 0, canvas.width, canvas.height)
+    } finally {
+        requestAnimationFrame(_ => draw(canvas, ctx, originalElems))
+    }
+}
+
+function getAudioStream(originalVideo) {
+    let audioContext = new AudioContext()
+    let dest = audioContext.createMediaStreamDestination()
+    let originalAudio = audioContext.createMediaElementSource(originalVideo)
+    originalAudio.connect(dest)
+    return dest.stream
+}
+
+main()
